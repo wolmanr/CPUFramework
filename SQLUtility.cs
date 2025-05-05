a7575ea509c9f2d0614545352d02ee40057e17dc
wolmanr <racheliwolman0462@gmail.com>
2025-05-05 11:01:36 -0400

    intial commit

---
+++ SQLUtility.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace CPUFramework
+{
+    public class SQLUtility
+    {
+        public static string connectionstring = "Server=.\\\\SqlExpress;Database=RecordKeeperDB;Trusted_Connection=true\";";
+        public  static DataTable GetDataTable(string sqlstatement)
+        {
+            Debug.Print(sqlstatement);
+            DataTable dt = new();
+            SqlConnection conn = new();
+            conn.ConnectionString = connectionstring;
+            conn.Open();
+            var cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = sqlstatement;
+            var dr = cmd.ExecuteReader();
+            dt.Load(dr);
+            return dt;
+        }
+    }
+}
