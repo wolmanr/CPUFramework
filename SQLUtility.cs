32fb3ce0987fffc14bf95bdcccdf2543b7cd3a3a
wolmanr <racheliwolman0462@gmail.com>
2025-05-05 20:08:51 -0400

    more recipe app work

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -14,7 +14,7 @@ namespace CPUFramework
 {
     public class SQLUtility
     {
-        public static string connectionstring = "Server=.\\\\SqlExpress;Database=RecordKeeperDB;Trusted_Connection=true\";";
+        public static string connectionstring = "Server=.\\SQLExpress;Database=RecipeDB;Trusted_Connection=true;TrustServerCertificate=True";
         public  static DataTable GetDataTable(string sqlstatement)
         {
             Debug.Print(sqlstatement);
