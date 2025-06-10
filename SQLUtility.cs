8177be7935a4737c021d15b44f1fcb601b267bdc
wolmanr <racheliwolman0462@gmail.com>
2025-06-10 11:40:51 -0400

    p

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -54,6 +54,22 @@ namespace CPUFramework
                 }
             }
         }
+
+        public static int GetFirstColumnFirstRowValue(string sql)
+        {
+            int n = 0;
+            DataTable dt = GetDataTable(sql);
+            if(dt.Rows.Count > 0 && dt.Columns.Count > 0)
+            {
+                if (dt.Rows[0][0] != DBNull.Value)
+                {
+                    int.TryParse(dt.Rows[0][0].ToString(), out n);
+                }
+            }
+            return n;
+
+
+        }
     }
 }
 //note
